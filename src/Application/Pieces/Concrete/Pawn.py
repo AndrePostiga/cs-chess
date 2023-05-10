25ea5847636b0231cc12aa06a4a4fcdcf49eaa2a
gabricrn <97262284+gabricrn@users.noreply.github.com>
2023-05-09 21:42:45 -0300

    Diagrama de classes completo (#10)
    * Diagrama de classes completo
    
    implementado, pronto para receber código
    
    * Nomes modificados
    
    feito para ficar de acordo com o modelo IEEE
    
    Faltam ainda modificações para deixar completamente de acordo com IEEE

--- src/Application/Pieces/Concrete/Pawn.py
+++ src/Application/Pieces/Concrete/Pawn.py
@@ -1,2 +1,12 @@
-class Pawn:
-    pass
+from src.Application.Pieces.Piece import Piece
+
+
+class Pawn(Piece):
+    def __init__(self, x_pos: int, y_pos: int):
+        super().__init__(x_pos, y_pos)
+        self.movementType = "frontal"
+        self.nMovement = 1
+
+    def move(self, table, i, j):
+        pass
+
