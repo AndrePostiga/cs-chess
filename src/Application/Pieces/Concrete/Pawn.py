6f75b5e5bfef60449c4295b8b3f2d8a7b930899f
gabricrn <gmills@id.uff.br>
2023-06-09 16:47:11 -0300

    En Passant implementado, não testado

--- src/Application/Pieces/Concrete/Pawn.py
+++ src/Application/Pieces/Concrete/Pawn.py
@@ -14,9 +14,18 @@ PAWN_IMG_PATH_B = \
                  "imgs", "testassets", "Chess_pdt60.png")
 
 
+def check_pawn(x, y, type, pieces):
+    for piece in pieces:
+        if isinstance(piece, Pawn) and piece.enpassant \
+                and piece.x == x and piece.y == y and piece.type != type:
+            return piece
+    return None
+
+
 class Pawn(Piece):
     def __init__(self, radius, x=0, y=0, ptype=0, offSetX=0, offSetY=0):
         super().__init__(radius, x, y, ptype, offSetX, offSetY)
+        self.enpassant = False
         if self.type == 1:
             self.image = Image(PAWN_IMG_PATH_W)
         else:
@@ -44,5 +53,46 @@ class Pawn(Piece):
                     maskmatrix[piece.x][piece.y] = -2
             else:
                 maskmatrix[piece.x][piece.y] = -2
+        #####en passant
+        self.check_enpassant(pieces, maskmatrix)
+
         maskmatrix[self.x][self.y] = 0
         return maskmatrix
+
+    def move(self, x, y, pieces):
+        fp = self.firstplay
+        cord = [self.x, self.y]
+        if self.movepossibilities(pieces)[x][y] == 3:
+            # enpassant
+            self.x = x
+            self.y = y
+            self.setCenter()
+            self.image.set_position(self.center[0], self.center[1])
+            self.firstplay = False
+
+            kp = check_pawn(self.x, self.y +
+                            (-1 if self.type == 0 else 1), self.type, pieces)
+            mv = True
+
+
+
+        else:
+            kp, mv = super().move(x, y, pieces)
+            # se for a primeira vez que moveu:
+
+            if mv and fp and (cord[x][y] == y + 2 or cord[x][y] == y - 2):
+                self.enpassant = True
+            else:
+                self.enpassant = False
+
+        #
+
+        return kp, mv
+
+    def check_enpassant(self, pieces, maskmatrix):
+        for i in [-1, 1]:
+            pawn = check_pawn(self.x + i, self.y, self.type, pieces)
+            if (pawn and
+                    maskmatrix[self.x + i][self.y + (1 if self.type == 0 else -1)] == -1):
+                maskmatrix[self.x + i][self.y + (1 if self.type == 0 else -1)] = 3
+                # 3 = special movement
